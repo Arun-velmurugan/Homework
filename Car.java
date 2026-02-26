096f57e0aeeadf508c55ee8ec0e18ae51d436143
Arun-velmurugan <arunrajvel02@gmail.com>
2026-02-26 12:11:59 +0530

    Homework

---
+++ Car.java
@@ -0,0 +1,18 @@
+class Car
+{
+	public static void main(String[] args)
+	{
+		String carName = "Tesla Model S";
+		short mileage = 396;
+		String fuelType = "Electric";
+		double price = 85000.00;
+		String brand = "Tesla";
+		System.out.println("Car Name :" + carName);
+		System.out.println("Brand :" + brand);
+		System.out.println("Fuel Type :" + fuelType);
+		System.out.println("Mileage :" + mileage +" miles");
+		System.out.println("Price :" + price);
+	}
+	
+	
+}
---
+++ Food1.java
@@ -0,0 +1,20 @@
+class Food1
+{
+	public static void main(String[] args)
+	{
+		String foodName = "Paneer Butter Masala";
+		short calories = 400;
+        boolean isVegeterian = true;
+		double price = 250.50;
+		System.out.println("Food Name :"+ foodName);
+		System.out.println("Cuisine Type :"+ "Indian");
+		System.out.println("Price :"+ price);
+		System.out.println("Vegeterian :"+ isVegeterian);
+		
+	}
+
+
+
+}
+
+
