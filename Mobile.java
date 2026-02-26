98bada4977d09cf9354e845f838592f30a33182d
Arun-velmurugan <arunrajvel02@gmail.com>
2026-02-26 11:59:05 +0530

    For Homework

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+class Mobile
+{
+	public static void main(String[] args)
+	{
+		String mobileName = "iphone 15 Pro";
+		String mobileBrand = "Apple";
+		double screenSize = 6.1;
+		short batteryCapacity = 3274;
+		double price = 134999.99;
+		System.out.println(mobileName);
+		System.out.println(mobileBrand);
+		System.out.println(screenSize);
+		System.out.println(batteryCapacity);
+		System.out.println(price);
+	}
+
+
+}
